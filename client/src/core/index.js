7dc41cf1585194bf97bb619c4bccf86957837d4d
YakirBar <yakirbar7820@gmail.com>
2024-07-12 16:06:30 +0300

    Client Layout

--- client/src/core/index.js
+++ client/src/core/index.js
@@ -1,6 +1,25 @@
+import "./index.css";
+import { Auth } from "./auth";
+import { Layout } from "./layout";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 const Core = () => {
+    const theme = createTheme({
+        palette: {
+            background: {
+                default: "#F0F4FC"
+            }
+        }
+    });
+
     return (
-        <h1>Core React</h1>
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <Auth />
+            </Layout>
+            <CssBaseline />
+        </ThemeProvider>
     );
 };
 
