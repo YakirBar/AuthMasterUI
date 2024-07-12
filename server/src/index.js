388da975334b05ad6b95c8613c1094f3910d26d1
YakirBar <yakirbar7820@gmail.com>
2024-07-12 22:08:14 +0300

    Server Authentication

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,14 @@
+const ip = require("ip");
+
 const http = require("http");
 
 const { app, port } = require("./core");
 
+const { connectLocalMongoDB } = require("./database/configs");
+
 http
   .createServer(app)
   .listen(port, () => {
+    connectLocalMongoDB();
     console.log(`server listening on port ${port}`);
   });
